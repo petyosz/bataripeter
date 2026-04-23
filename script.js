f44b2bc0da32b31c7c0ee55e2899e0f1896c876d
petyosz <kulamancs123@gmail.com>
2026-04-23 09:32:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,28 @@ const menuBtn = document.getElementById("menuBtn");
 const menu = document.getElementById("menu");
 
 menuBtn.onclick = () => {
-    menu.style.display = menu.style.display === "block" ? "none" : "block";
+    menu.classList.toggle("active");
 };
 
 const pdfMap = {
     "Elektronika": "Batari_Peter_Elektronika.pdf",
-    "Programfejlesztes": "Batari_Peter_13B_programfejlesztes.pdf",
+    "Programfejlesztes": "Batari_Peter_programozasalapjai.pdf",
     "IoT": "IOT_1.pdf"
 };
 
 function openPDF(name){
     const file = pdfMap[name];
+
     if(!file){
         alert("Nincs PDF!");
         return;
     }
 
     document.getElementById("frame").src = "pdfs/" + file;
-    document.getElementById("pdf").style.display = "block";
+    document.getElementById("pdf").classList.add("show");
 }
 
 function closePDF(){
-    document.getElementById("pdf").style.display = "none";
+    document.getElementById("pdf").classList.remove("show");
     document.getElementById("frame").src = "";
 }
